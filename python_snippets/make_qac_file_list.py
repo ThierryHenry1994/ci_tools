1976c188a18efadbe99917d60f1e59ff6989f8fd
guojun <jun.guo@bitech-automotive.com>
2021-04-06 17:07:18 +0800

    add make qac file list
    Change-Id: I23044eb67f755675b25716dce773a9ca86719a80

---
+++ python_snippets/make_qac_file_list.py
@@ -0,0 +1,16 @@
+import os
+import fire
+
+
+def get_file_from_project(project_path, file_path):
+    for root, ds, fs in os.walk(project_path):
+        for f in fs:
+            if f.endswith('.cpp') or f.endswith(".c"):
+                fullname = os.path.join(root, f)
+                with open(file_path+"/"+'qac_filelist.txt', "a") as log:
+                    log.write(fullname+"\n")
+
+
+if __name__ == '__main__':
+    fire.Fire(get_file_from_project)
+
