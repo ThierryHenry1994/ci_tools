e60d942f90ca4f2a454c3f65f7d4c8924c77d7f5
guojun <jun.guo@bitech-automotive.com>
2021-04-06 10:39:18 +0800

    update fit ci job
    Change-Id: I8d48e80b5cd1dead9e4399ee6c3efcef7e35944b

--- python_snippets/analyze.py
+++ python_snippets/analyze.py
@@ -1,6 +1,9 @@
-def get_file_from_git_pull():
+import fire
+
+
+def get_file_from_git_pull(git_pull_log):
     file_list = []
-    text_file = open("test.txt", "r")
+    text_file = open(git_pull_log, "r")
     lines = text_file.readlines()
     for line in lines:
         if "|" in line:
@@ -10,11 +13,16 @@ def get_file_from_git_pull():
     return file_list
 
 
-def write_data(data_list):
-    with open("filelist.txt", "w") as f:
+def write_data(data_list, data_file):
+    with open(data_file, "w") as f:
         for data in data_list:
             f.write(data.strip()+"\n")
 
 
-list1 = get_file_from_git_pull()
-write_data(list1)
+def write_qac_file_list(git_log, qac_file):
+    change_list = get_file_from_git_pull(git_log)
+    write_data(change_list, qac_file)
+
+
+if __name__ == "__main__":
+    fire.Fire(write_qac_file_list)
