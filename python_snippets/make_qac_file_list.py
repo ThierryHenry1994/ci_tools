6a6b7b698abcd6c0a12f1ab3b489b45543817c9b
junguo <jun.guo@bitech-automotive.com>
2021-04-25 10:40:56 +0800

    update: del invalid files

--- python_snippets/make_qac_file_list.py
+++ python_snippets/make_qac_file_list.py
@@ -6,9 +6,10 @@ def get_file_from_project(project_path, file_path):
     for root, ds, fs in os.walk(project_path):
         for f in fs:
             if f.endswith('.cpp') or f.endswith(".c"):
-                fullname = os.path.join(root, f)
-                with open(file_path+"/"+'qac_filelist.txt', "a") as log:
-                    log.write(fullname+"\n")
+                if f!="GPT_cfg.c" and f!="display.c" and f!="FP_cfg.c":
+                    fullname = os.path.join(root, f)
+                    with open(file_path+"/"+'qac_filelist.txt', "a") as log:
+                        log.write(fullname+"\n")
 
 
 if __name__ == '__main__':
