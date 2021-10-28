f97f9b6d1a025748a2aeb951c217e8e599096feb
junguo <jun.guo@bitech-automotive.com>
2021-10-28 10:24:07 +0800

    update；pipeline cmake script
    Change-Id: Ib6661fe67440d03adaacd2c73edf960a0d53ab95

--- pipeline_snippets/cmake.bat
+++ pipeline_snippets/cmake.bat
@@ -1,11 +1,10 @@
 rem @echo off
-rd /s /q code
+set WorkSpace=%1%
 rd /s /q code_build_target
 rd /s /q code_build_target_release
-set Version=%1%
 set COMPILE_KIND=%2%
 
-git clone "ssh://10.179.48.85:29418/projects/chery/8155_convergence/mcu" -b %Version% code
+cd %WorkSpace%
 cd code
 
 echo "#### build.log start ####"
