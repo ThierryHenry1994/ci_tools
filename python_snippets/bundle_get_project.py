f1722a77e762eb0f5f2e48038652f63ae43561fa
junguo <jun.guo@bitech-automotive.com>
2021-11-29 17:38:03 +0800

    update:fix ALL build in bundle failed
    Change-Id: Ie0e9763c845bf2d73cccf04a9d39c223fa4d9b2d

--- python_snippets/bundle_get_project.py
+++ python_snippets/bundle_get_project.py
@@ -21,13 +21,17 @@ def write_project_file(source_file,target_file):
                 print(project)
                 for i in project:
                     if "project" in i:
-                        name = i.split(":")[1].split(",")
-                        print(name)
-                        for j in name:
-                            if j in bundle_list:
-                                f.write(j + "\n")
-                            else:
-                                raise BaseException("param not in project name list!!! pls check it")
+                        if "ALL" in i:
+                            for bundle in bundle_list:
+                                f.write(bundle + "\n")
+                        else:
+                            name = i.split(":")[1].split(",")
+                            print(name)
+                            for j in name:
+                                if j in bundle_list:
+                                    f.write(j + "\n")
+                                else:
+                                    raise BaseException("param not in project name list!!! pls check it")
 
 #write_project_file("..\\git.txt","project.txt")
 fire.Fire(write_project_file)
